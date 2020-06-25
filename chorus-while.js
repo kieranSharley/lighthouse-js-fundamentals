609b8c922433bf8827afa6f9fc8042d87f2ac0a0
kieranSharley <ksharley9@gmail.com>
2020-06-25 01:46:07 +0000

    First function Hello, world

--- chorus-while.js
+++ chorus-while.js
@@ -5,3 +5,14 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
+
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
---
+++ hello.js
@@ -0,0 +1,8 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
